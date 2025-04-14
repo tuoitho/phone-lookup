caf8409d5916be55e73ca3affd155ab0af366c60
Lê Đình Trí <135036590+tuoitho@users.noreply.github.com>
2025-04-14 17:17:53 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,9 +21,9 @@ const sqlConfig = {
   database: process.env.DB_NAME ,
   server: process.env.DB_SERVER,
   pool: {
-    max: 10,
-    min: 1,
-    idleTimeoutMillis: 300000000
+    max: 20,
+    min: 4,
+    idleTimeoutMillis: 30000000
   },
   options: {
     encrypt: true, // for Azure
